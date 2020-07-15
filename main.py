9480d22ad1e2765c6ce5447549fb156c18bd1a79
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-15 15:14:39 +0000

    Worked on.

---
+++ main.py
@@ -0,0 +1,26 @@
+example = "example"
+print(example)
+
+name = "Elia Deppe"
+print(name)
+
+favartist = "Earl Sweatshirt"
+print(favartist)
+
+vacation = "Nosara"
+print(vacation)
+
+hobby = "Painting and Skateboarding"
+print(hobby)
+
+TodaysDate = "06/20/2020"
+print(TodaysDate)
+
+a = "example" 
+b = "name" 
+c = "favartist"
+d = "vacation"
+e = "hobby"
+f = "TodaysDate"
+
+print ( " This is an " + a + ". My name is "+ b + ". My favartist is " + c + ". I want to vacation to " + d + ". My hobby is " + e + ". and TodaysDate is" + f +".")
